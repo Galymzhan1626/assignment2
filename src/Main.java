94e1750b2afa8b8ef29edf8fdf3df6584effb434
Galymzhan1626 <231169@astanait.edu.kz>
2024-01-29 09:00:29 +0600

    This Java code defines three classes Person, Employee, and Student. The code demonstrates inheritance, interface implementation.

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Person> people = new ArrayList<>();
-        people.add(new Employee("John", "Lennon", "Manager", 27045.78));
-        people.add(new Employee("George", "Harrison", "Developer", 50000.00));
-        people.add(new Student("Ringo", "Starr", 3.5));
-        people.add(new Student("Paul", "McCartney", 2.0));
+        people.add(new Employee("Galymzhan", "Aliakbar", "Manager", 27045.78));
+        people.add(new Employee("Adlet", "Baigaliev", "Developer", 50000.00));
+        people.add(new Student("Arman", "Erniyaz", 3.5));
+        people.add(new Student("Alibi", "Batyrkhan", 2.0));
 
         printData(people);
     }
